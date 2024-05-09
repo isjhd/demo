b4d0717f58970ecb4f57469e2d99c6bf9e45673b
阿良良木君 <3478215435@qq.com>
2024-05-09 15:15:47 +0800

    second commit

--- src/main/java/com/example/demo/git/GitTest.java
+++ src/main/java/com/example/demo/git/GitTest.java
@@ -3,5 +3,9 @@ package com.example.demo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+
     }
 }
