9e7245dbc23f4ee2d979f94b3baa448bf830de2c
阿良良木君 <3478215435@qq.com>
2024-05-09 17:22:41 +0800

    Update GitTest.java
    无

--- src/main/java/com/example/demo/git/GitTest.java
+++ src/main/java/com/example/demo/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git hot-fix!");
         System.out.println("hello git hot-fix2!");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
